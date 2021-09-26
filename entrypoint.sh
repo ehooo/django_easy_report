070614cc39dc4a3eb62ed3766bd431ee18cc95d4
ehooo <web.ehooo@gmail.com>
2021-09-26 20:37:38 +0200

    More verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 if ! test -f "db.sqlite3"; then
+  echo "Create migrations"
   python manage.py makemigrations
+  echo "Apply migrations"
   python manage.py migrate
-  python manage.py loaddata initial_data
+  echo "Load basic data"
+  python manage.py loaddata basic_data
+  echo "Create Admin account"
   DJANGO_SUPERUSER_PASSWORD=1234 python manage.py createsuperuser --username=admin --email="admin@localhost" --skip-checks --noinput
 fi
 python manage.py runserver 0.0.0.0:8000
