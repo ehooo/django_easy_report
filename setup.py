e76ec4256ed371db44889c332787be23e4e33e27
Victor Torre <web.ehooo@gmail.com>
2021-10-16 17:33:25 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="django-easy-report",
-    version="0.0.1",
+    version="0.0.2",
     author="Victor Torre",
     author_email="web.ehooo@gmail.com",
     description="Django app for report generation",
