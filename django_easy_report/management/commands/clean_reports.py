7db9f9a8965072d2470a19fd7362384352afe38f
ehooo <web.ehooo@gmail.com>
2021-12-18 01:24:35 +0100

    Improve clean_reports command args parser

--- django_easy_report/management/commands/clean_reports.py
+++ django_easy_report/management/commands/clean_reports.py
@@ -1,29 +1,37 @@
 from datetime import datetime, timedelta
+import argparse
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from django_easy_report.models import ReportQuery
+
+
+def valid_date(str_date):
+    try:
+        return datetime.strptime(str_date, "%d-%m-%Y")
+    except ValueError:
+        msg = "Invalid date format, use DD-MM-YYYY.".format(str_date)
+        raise argparse.ArgumentTypeError(msg)
 
 
 class Command(BaseCommand):
     help = 'Clean old reports'
 
     def add_arguments(self, parser):
-        parser.add_argument('--until', type=str,
-                            help='Date in format DD-MM-YYYY')
-        parser.add_argument('--weeks', type=int)
-        parser.add_argument('--days', type=int)
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument('--until',
+                           type=valid_date,
+                           help='Date in format DD-MM-YYYY')
+        group.add_argument('--weeks', type=int)
+        group.add_argument('--days', type=int)
 
     def handle(self, *args, **options):
         until = None
-        if options['until']:
-            try:
-                until = datetime.strptime(options['until'], '%d-%m-%Y')
-            except ValueError:
-                raise CommandError('Invalid date {}'.format(options['until']))
-        elif options['weeks']:
+        if options['until'] is not None:
+            until = options['until']
+        elif options['weeks'] is not None:
             until = datetime.today() - timedelta(weeks=options['weeks'])
-        elif options['days']:
+        elif options['days'] is not None:
             until = datetime.today() - timedelta(days=options['days'])
 
         reports = ReportQuery.objects.filter(created_at__lt=until)
